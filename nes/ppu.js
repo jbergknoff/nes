3f9a72a500eb06baa059a683a2660c91e72eb34f
jbergknoff <jonathan@bergknoff.com>
2014-05-12 21:52:40 -0700

    Revert "cleanup"
    This reverts commit 6ba58f92472574ba173d4707c39a06556a4d83ab.

--- nes/ppu.js
+++ nes/ppu.js
@@ -2,27 +2,24 @@ var NES = NES || {};
 
 var Bitmasks =
 {
-	// $2000
+	// Bitmasks for use with $2000
 	"NAMETABLE_ADDRESS": 0x03,
 	"VRAM_INCREMENT_32": 0x04,
 	"PATTERN_TABLE_ADDRESS": 0x10,
 	"TALL_SPRITES": 0x20,
 	"GENERATE_NMI": 0x80,
 
-	// $2001
+	// Bitmasks for use with $2001
 	"GRAYSCALE": 0x01,
 	"SHOW_BG_LEFT_EDGE": 0x02,
 	"SHOW_SPRITES_LEFT_EDGE": 0x04,
 	"SHOW_BG": 0x08,
 	"SHOW_SPRITES": 0x10,
 
-	// $2002
+	// Bitmasks for use with $2002
 	"SPRITE_OVERFLOW": 0x20,
 	"SPRITE_ZERO_HIT": 0x40,
-	"IN_VBLANK": 0x80,
-
-	// Sprite mask
-	"SPRITE_ZERO": 0x40
+	"IN_VBLANK": 0x80
 };
 
 Bitmasks.SHOW_SOMETHING = Bitmasks.SHOW_BG | Bitmasks.SHOW_SPRITES;
@@ -151,7 +148,7 @@ NES.PPU = function(Options)
 		{
 			for (var Y = SpriteZeroY; Y < SpriteZeroY + SpriteHeight; Y++)
 			{
-				if (!(SpritePixels[256 * Y + X] & Bitmasks.SPRITE_ZERO)) continue;
+				if (!(SpritePixels[256 * Y + X] & 0x40)) continue;
 				if (!(R2001 & Bitmasks.SHOW_BG_LEFT_EDGE) && X < 8) continue;
 				if (GetBGPixel(Y, X) == 0) continue;
 
@@ -572,7 +569,7 @@ NES.PPU = function(Options)
 						// SpriteZeroPixelData stores a stencil of how sprite #0 appears on the screen, false for transparent pixel, true otherwise.
 						if (i == 0)
 						{
-							SpritePixels[256 * (ScreenY + ScreenOffsetY) + (ScreenX + ScreenOffsetX)] |= Bitmasks.SPRITE_ZERO;
+							SpritePixels[256 * (ScreenY + ScreenOffsetY) + (ScreenX + ScreenOffsetX)] |= 0x40;
 
 							// Double-check that sprite #0 should be doing what follows this block.
 							// Namely, if sprite #0 is a BG sprite and there's already a non-BG pixel in place, skip.
@@ -581,7 +578,7 @@ NES.PPU = function(Options)
 						}
 
 						PaletteIndex = LowColor | ((Attributes << 2) & 0x0C);
-						SpritePixels[256 * (ScreenY + ScreenOffsetY) + (ScreenX + ScreenOffsetX)] = (PaletteIndex | (Background ? 0x80 : 0) | (i == 0 ? Bitmasks.SPRITE_ZERO : 0)) & 0xFF;
+						SpritePixels[256 * (ScreenY + ScreenOffsetY) + (ScreenX + ScreenOffsetX)] = (PaletteIndex | (Background ? 0x80 : 0) | (i == 0 ? 0x40 : 0)) & 0xFF;
 					}
 				}
 			}
